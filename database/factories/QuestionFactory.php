84358355799d71d74e5d5b40e950ebb69887769c
Edomwande Ikponmwosa George <iedomwande@yahoo.com>
2020-05-27 20:20:48 +0100

    Refactor Factory, Migrations, Seeding

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -4,9 +4,21 @@
 
 use App\Model\Question;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Model\Category;
+use App\User;
 
 $factory->define(Question::class, function (Faker $faker) {
-    return [
-        //
+        $title = $faker->sentence;
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'body' => $faker->text,
+            'category_id' => function () {
+                return Category::all()->random();
+            },
+            'user_id' => function () {
+                return User::all()->random();
+            }
     ];
 });
--- database/migrations/2020_05_27_180633_create_questions_table.php
+++ database/migrations/2020_05_27_180633_create_questions_table.php
@@ -18,8 +18,8 @@ class CreateQuestionsTable extends Migration
             $table->string('title');
             $table->string('slug');
             $table->text('body');
-            $table->integer('category_id')->unsigned();
-            $table->integer('user_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
             $table->timestamps();
         });
     }
