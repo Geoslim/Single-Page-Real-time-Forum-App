f81800af0872d93e29bf7d5dc4c5bf0132ab7f6b
Edomwande Ikponmwosa George <iedomwande@yahoo.com>
2020-05-28 14:05:22 +0100

    Adding JWT

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,27 @@
 <?php
 
 Route::apiResource('questions', 'QuestionController');
+
 Route::apiResource('categories', 'CategoryController');
+
 Route::apiResource('/questions/{question}/reply', 'ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@like');
+Route::delete('/like/{reply}', 'LikeController@unlike');
+
+
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
